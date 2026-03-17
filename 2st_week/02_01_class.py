1d9a6506e4674fb1f9a9cc323aa7b5361f9c3181
박현진 <hyunjin@petch.com>
2026-03-18 07:26:42 +0900

    feat: 링크드 리스트 append() 구현하기

--- 2st_week/02_01_class.py
+++ 2st_week/02_01_class.py
@@ -1,5 +1,5 @@
 class Person:
-    def __init__(self, name_param): # self는 자기자신의 값
+    def __init__(self, name_param): # 생성자, self는 자기자신의 값
         self.name = name_param
         print("hi hi", self)
 
@@ -8,10 +8,10 @@ class Person:
 #    pass # 아무런 내용이 없다는 의미
 
 
-person_1 = Person("유재석") # 클래스를 통해 하나의 객체를 만들겠다는 의미
-print(person_1) # <__main__.Person object at 0x1026967b0>
-print(person_1.name)
-person_1.talk()
+person_1 = Person("유재석") # 클래스 생성자를 통해 하나의 객체를 만들겠다는 의미, hi hi <__main__.Person object at 0x1024667b0>
+print(person_1) # <__main__.Person object at 0x1024667b0>
+print(person_1.name) # 유재석
+person_1.talk() # hi hi 유재석 입니다.
 
 person_2 = Person("박명수") # (): 생성자
 print(person_2) # <__main__.Person object at 0x102680f50>
